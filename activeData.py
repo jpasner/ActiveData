363096984f90c61a6834f07a42c1b03992157ad7
jpasner <jakepasner@gmail.com>
2020-06-28 21:44:29 -0700

    Expand flask to two pages

--- activeData.py
+++ activeData.py
@@ -3,10 +3,14 @@ from flask import Flask, render_template
 #Create Flask object 'app' with special variable __name__
 app = Flask(__name__)
 
-# Define a function which runs when the user navigates to home '/' or localhost:5000 if run privately
+# Define routes and html for desired pages
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
 
 # __name__ == __main__ when the script is executed directly as opposed to being imported
 if __name__ == '__main__':
