612c9bf8c6ec6ec5d75909c7da9d5b3986ee806b
jpasner <jakepasner@gmail.com>
2020-06-28 21:24:39 -0700

    Add comments as I learn

--- activeData.py
+++ activeData.py
@@ -1,9 +1,13 @@
 from flask import Flask
 
+#Create Flask object 'app' with special variable __name__
 app = Flask(__name__)
 
+# Define a function which runs when the user navigates to home '/' or localhost:5000 if run privately
 @app.route('/')
-def hello_world():
-  return 'Hello from Flask!'
+def home():
+    return 'Hello World!'
+
+# __name__ == __main__ when the script is executed directly as opposed to being imported
 if __name__ == '__main__':
-  app.run()
+    app.run(debug=True)
