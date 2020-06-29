9105950e690c2dffce6623b1050935d4e0db7946
jpasner <jakepasner@gmail.com>
2020-06-28 21:37:54 -0700

    Expand flask app to display home.html when user navigates to '/'

--- activeData.py
+++ activeData.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 #Create Flask object 'app' with special variable __name__
 app = Flask(__name__)
@@ -6,7 +6,7 @@ app = Flask(__name__)
 # Define a function which runs when the user navigates to home '/' or localhost:5000 if run privately
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return render_template('home.html')
 
 # __name__ == __main__ when the script is executed directly as opposed to being imported
 if __name__ == '__main__':
